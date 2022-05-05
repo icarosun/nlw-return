f38bf4a5a87294e0832b18d9ff4ddde97b9a8de8
Ícaro <isp.snf18@uea.edu.br>
2022-05-04 23:26:16 -0400

    add: close button

--- web/src/components/Widget.tsx
+++ web/src/components/Widget.tsx
@@ -10,8 +10,7 @@ export function Widget() {
       <Popover.Button className="bg-violet-500 rounded-full px-3 h-12 text-white flex items-center group">
         <ChatTeardropDots className="w-6 h-6" />
 
-        <span className="max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear">
-          <span className="pl-2"></span>
+        <span className="max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear"> <span className="pl-2"></span>
           Feedback
         </span>
       </Popover.Button>
--- web/src/components/WidgetForm.tsx
+++ web/src/components/WidgetForm.tsx
@@ -1,14 +1,32 @@
+import { CloseButton } from "./CloseButton";
+
+const feedbackTypes = {
+  BUG: {
+    title: 'Problema'
+  },
+  IDEA: {
+    title: 'Ideia'
+  },
+  OTHER: {
+    title: 'Outro'
+  }
+
+};
+
 export function WidgetForm() {
   return (
     <div className="bg-zinc-900 p-4 relative rounded-2xl nb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem) md:w-auto">
       <header>
         <span className="text-xl leading-6">Deixe seu feedback</span>
+        <CloseButton />
       </header>
 
-      <p>Hello World</p>
+      <div className="flex pv-8 gap-2 w-full">
+
+      </div>
 
       <footer className="text-xs text-neutral-400">
-        Feito com ♥ pela <a className="underline underline-offset-2" href="https://rockeatseat.com.br">Rocketseat</a>
+        Feito com ♥ pela <a className="underline underline-offset-2">Rocketseat</a>
       </footer>
     </div>
   );
