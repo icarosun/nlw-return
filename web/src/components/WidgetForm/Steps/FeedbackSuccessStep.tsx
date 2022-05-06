e6e36e44078956b4c1e67db6a81a4dec134a9d1a
Ícaro <isp.snf18@uea.edu.br>
2022-05-06 00:03:41 -0400

    finished: stage 2

--- web/src/components/WidgetForm/Steps/FeedbackSuccessStep.tsx
+++ web/src/components/WidgetForm/Steps/FeedbackSuccessStep.tsx
@@ -1,3 +1,35 @@
-export function FeedbackSuccessStep() {
-  return (null)
+import { CloseButton } from "../../CloseButton";
+import successImageUrl from "../../../assets/success.svg";
+
+interface onFeedbackSentProps {
+  onFeedbackRestartRequested: () => void;
+}
+
+export function FeedbackSuccessStep({
+  onFeedbackRestartRequested
+}: onFeedbackSentProps) {
+  return (
+    <>
+      <header>
+        <CloseButton />
+      </header>
+
+      <div className="flex flex-col py-10 w-[304px] items-center">
+        <img src={successImageUrl} alt="Feedback enviado com sucesso!" />
+
+        <span className="text-xl mt-2">
+          Agradecemos o feedback!
+        </span>
+
+        <button
+          type="button"
+          className="py-2 px-6 mt-6 bg-zinc-800 rounded-md border-transparent text-sm leading-6 hover:bg-zinc-700 transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500"
+          onClick={onFeedbackRestartRequested}
+        >
+          Quero enviar outro
+        </button>
+
+      </div>
+    </>
+  );
 }
--- web/src/components/WidgetForm/index.tsx
+++ web/src/components/WidgetForm/index.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 
+import { FeedbackSuccessStep } from './Steps/FeedbackSuccessStep';
 import { FeedbackTypeStep } from './Steps/FeedbackTypeStep';
 import { FeedbackContentStep } from './Steps/FeedbackContentStep';
 
@@ -36,20 +37,31 @@ export type FeedbackType = keyof typeof feedbackTypes;
 
 export function WidgetForm() {
   const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null);
+  const [feedbackSent, setFeedbackSent] = useState(false);
 
   function handleRestartFeedback() {
+    setFeedbackSent(false);
     setFeedbackType(null);
   }
 
   return (
     <div className="bg-zinc-900 p-4 relative rounded-2xl nb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem) md:w-auto">
-      {!feedbackType ? (
-        <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
-      ) : (
-        <FeedbackContentStep
-          feedbackType={feedbackType}
+      {feedbackSent ? (
+        <FeedbackSuccessStep
           onFeedbackRestartRequested={handleRestartFeedback}
         />
+      ) : (
+        <>
+          {!feedbackType ? (
+            <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
+          ) : (
+            <FeedbackContentStep
+              feedbackType={feedbackType}
+              onFeedbackRestartRequested={handleRestartFeedback}
+              onFeedbackSent={() => setFeedbackSent(true)}
+            />
+          )}
+        </>
       )}
 
       <footer className="text-xs text-neutral-400">
