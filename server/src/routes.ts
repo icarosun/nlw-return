f513e2db6df5ff76a5bef33c51e8271701c6c22c
Ícaro <isp.snf18@uea.edu.br>
2022-05-07 21:53:31 -0400

    refactor: using SOLID

---
+++ server/src/routes.ts
@@ -0,0 +1,41 @@
+import express from 'express';
+import nodemailer from 'nodemailer'
+import { prisma } from './prisma'
+
+export const routes = express.Router();
+
+var transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "8daaa024c205f0",
+    pass: "465853e464d36e"
+  }
+});
+
+routes.post('/feedbacks', async (req, res) => {
+  const { type, comment, screenshot } = req.body;
+
+  const feedback = await prisma.feedback.create({
+    data: {
+      type,
+      comment,
+      screenshot
+    }
+  })
+
+  transport.sendMail({
+    from: 'Equipe Feedget <oi@feedget.com>',
+    to: 'Ícaro Santos <ica.spereira12@gmail.com>',
+    subject: 'Novo feedback',
+    html: [
+      '<div style="font-family: sans-serif; font-size: 16px; color:#111;">',
+      `<p>Tipo de feedback: ${type}</p>`,
+      `<p>Comentário: ${comment}</p>`,
+      '</div>'
+    ].join('\n')
+  })
+
+  return res.status(201).json({ data: feedback });
+})
+
