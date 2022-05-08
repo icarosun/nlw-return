107445475b7f05a565993c9033b567753d847f65
Ícaro <isp.snf18@uea.edu.br>
2022-05-08 18:24:25 -0400

    add: bottomSheet

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,4 +1,8 @@
-import AppLoading from 'expo-app-loading';
+import React from 'react';
+import 'react-native-gesture-handler';
+import * as SplashScreen from 'expo-splash-screen';
+
+import { StatusBar } from 'expo-status-bar';
 
 import {
   useFonts,
@@ -6,34 +10,37 @@ import {
   Inter_500Medium
 } from '@expo-google-fonts/inter';
 
-import { StatusBar } from 'expo-status-bar';
 import { View } from 'react-native';
-
 import { theme } from './src/theme';
-import { Widget } from './src/components/Widget';
+import Widget from './src/components/Widget';
 
 export default function App() {
+  SplashScreen.preventAutoHideAsync();
+
   const [fontsLoaded] = useFonts({
     Inter_400Regular,
     Inter_500Medium
   });
 
   if (!fontsLoaded) {
-    return <AppLoading />;
+    return null;
   }
+
+  SplashScreen.hideAsync();
 
   return (
     <View style={{
       flex: 1,
       backgroundColor: theme.colors.background
     }}>
-      <Widget />
 
       <StatusBar
         style="light"
         backgroundColor="transparent"
         translucent
       />
+
+      <Widget />
     </View>
   );
 }
